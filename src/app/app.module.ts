bbe7a8f321f310fb9249fefb7f8f1862d6c8ed8b
AndreCRibeiro <andre_ribeiro97@hotmail.com>
2018-05-12 17:15:20 -0300

    Aplicativo mostrando única página de filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MenuInicialPageModule } from '../pages/menu-inicial/menu-inicial.module';
 import { IntroPageModule } from '../pages/intro/intro.module';
+import { HttpModule } from "@angular/http" 
+import { MovieProvider } from '../providers/movie/movie';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
     BrowserModule,
     IonicModule.forRoot(MyApp), 
     MenuInicialPageModule,
-    IntroPageModule
+    IntroPageModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -38,7 +41,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MovieProvider
   ]
 })
 export class AppModule {}
--- src/pages/menu-inicial/menu-inicial.ts
+++ src/pages/menu-inicial/menu-inicial.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the MenuInicialPage page.
@@ -12,11 +13,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-menu-inicial',
   templateUrl: 'menu-inicial.html',
+  providers:
+  [
+    MovieProvider
+  ]
 })
 export class MenuInicialPage {
+  public objeto_feed = 
+  {
+    titulo:"Taís Rodrigues Sad",
+    data:"9 de março de 1997",
+    descricao:"Te amo Déco!",
+    qntd_likes: 12,
+    qntd_comments: 4,
+    time_comment: "11h atrás"
+  }
   public nome_usuario:string="Taís Rodrigues Sad";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public lista_filmes = new Array<any>();
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private movieProvider: MovieProvider
+  ) {
   }
 
   public somaDoisNumeros(num1:number,num2:number): void
@@ -25,7 +45,16 @@ export class MenuInicialPage {
   }
 
   ionViewDidLoad() {
-    //this.somaDoisNumeros(10,99);
+    this.movieProvider.getLatestMovies().subscribe(
+      data=>{
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+      console.log(objeto_retorno);
+    }, error=> {
+      console.log(error);
+    }
+  )
   }
 
 }
